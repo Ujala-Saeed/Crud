ed7555991d45d4b32d331a83124790c2a9264db6
Ujala-Saeed <ujalasaeed.46@gmail.com>
2019-07-15 09:28:29 +0500

    initial commit

---
+++ code.php
@@ -0,0 +1,50 @@
+<?php 
+	session_start();
+	$db = mysqli_connect('localhost', 'root', '', 'phone_directory');
+
+	// initialize variables
+	$f_name = "";
+	$l_name = "";
+	$email="";
+	$ph_no="";
+	$id = 0;
+	$edit_state = false;
+	$update = false;
+
+	if (isset($_POST['save'])) {
+		$f_name = $_POST['f_name'];
+		$l_name = $_POST['l_name'];
+		$email=$_POST['email'];
+		$ph_no=$_POST['ph_no'];
+
+		echo mysqli_query($db, "INSERT into ph_dir(`f_name`,`l_name`,`email`,`ph_no`)VALUES('$f_name','$l_name','$email','$ph_no')");
+		$_SESSION['message'] = "Record Saved"; 
+		header('location: index.php');
+	}
+//update records
+if (isset($_POST['update'])) {
+
+	$id = $_POST['id'];
+	$f_name = $_POST['f_name'];
+	$l_name = $_POST['l_name'];
+	$email = $_POST['email'];
+	$ph_no = $_POST['ph_no'];
+	
+	$q= "UPDATE ph_dir SET f_name='$f_name', l_name='$l_name', email='$email',ph_no='$ph_no' WHERE id='$id' ";
+	//$res = mysqli_query($db,$sql) or trigger_error(mysqli_error()." in ".$sql);
+	$check=mysqli_query($db, $q) or die("Cannot UPDATE Record." .mysqli_error($db));
+	$_SESSION['message'] = "Record updated!"; 
+	header('location: index.php');
+	
+}
+//delete records
+if (isset($_GET['del'])) {
+	$id = $_GET['del'];
+	mysqli_query($db, "DELETE FROM ph_dir WHERE id=$id");
+	$_SESSION['message'] = "Record deleted!"; 
+	header('location: index.php');
+}
+	
+//retrive records
+$results=mysqli_query($db,"SELECT * FROM ph_dir ");	
+?>
---
+++ index.php
@@ -0,0 +1,96 @@
+<?php
+include('code.php');
+//fetch the record to be updated
+if(isset($_GET['edit'])){
+$id =$_GET['edit'];
+$edit_state=true;
+$rec = mysqli_query($db,"SELECT * FROM ph_dir WHERE id=$id ");
+$record =mysqli_fetch_array($rec);
+$id= $record['id'];
+$f_name= $record['f_name'];
+$l_name= $record['l_name'];
+$email= $record['email'];
+$ph_no= $record['ph_no'];
+
+
+}
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Phone Directory</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php if (isset($_SESSION['message'])): ?>
+	<div class="msg">
+		<?php 
+			echo $_SESSION['message']; 
+			unset($_SESSION['message']);
+		?>
+	</div>
+<?php endif ?>
+	<table>
+	<thead>
+	<tr>
+	<th>ID</th>
+	<th>First Name</th>
+	<th>Last Name</th>
+	<th>Email</th>
+	<th>Phone #</th>
+	<th colspan="2">Action</th>
+	<tbody>
+	<?php while($row=mysqli_fetch_array($results)){?>
+	<tr>
+	<td><?php echo $row['id'];?></td>
+	<td><?php echo $row['f_name'];?></td>
+	<td><?php echo $row['l_name'];?></td>
+	<td><?php echo $row['email'];?></td>
+	<td><?php echo $row['ph_no'];?></td>
+	
+	<td>
+	<a class="edit_btn" href="index.php?edit=<?php echo $row['id']; ?>">Edit</a>
+	</td>
+	<td>
+	<a class="del_btn" href="code.php?del=<?php echo $row['id']; ?>">Delete</a>
+	</td>
+	
+	</tr>
+	<?php } ?>
+	</tbody>
+	</tr>
+	</thead>
+	</table>
+	<form method="post" action="code.php" >
+	<input type="hidden" name="id" value="<?php echo $id; ?>">
+		<div class="input-group">
+			<label>First Name</label>
+			<input type="text" name="f_name" value="<?php echo $f_name; ?>">
+		</div>
+		<div class="input-group">
+			<label>Last Name</label>
+			<input type="text" name="l_name" value="<?php echo $l_name; ?>">
+		</div>
+		<div class="input-group">
+			<label>Email</label>
+			<input type="text" name="email" value="<?php echo $email; ?>">
+		</div>
+		<div class="input-group">
+			<label>Phone #</label>
+			<input type="text" name="ph_no" value="<?php echo $ph_no; ?>">
+		</div>
+		<div class="input-group">
+			<?php if($edit_state==false): ?>
+			<button class="btn" type="submit" name="save" >Save</button>
+			<?php else: ?>
+			<button class="btn" type="submit" name="update" >Update</button>
+			<?php endif ?>
+			
+		</div>
+	</form>
+</body>
+
+</html>
